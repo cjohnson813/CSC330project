9a095c573405ee37b3ef618449183665aca41caa
Muhammad Onais <132248928+onais495@users.noreply.github.com>
2025-11-01 03:30:15 -0400

    Refactor requestHandler to use const for variables

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ let myserver = http.createServer(requestHandler);
 
 //function to handle requests
 function requestHandler(req, res) {
-    let parsedURL = url.parse(req.url,true); 
-    let pathName = parsedURL.pathname;
-    let query = parsedURL.query;
-    const fileName = "." + '/public_html/' + pathName;
+    const parsedURL = url.parse(req.url,true); 
+    const pathName = parsedURL.pathname;
+    const query = parsedURL.query;
+    let fileName = "." + '/public_html/' + pathName;
 	//route the request for empty pathname
 	if (pathname == '/')
 	{
