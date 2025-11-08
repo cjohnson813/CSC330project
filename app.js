1b9715b5228ef0deb2ec34198ce9e94a5bdbc612
onais495 <muhammadonais495@gmail.com>
2025-11-07 19:32:00 -0500

    added approved, denied and view functionality to backend

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ let events =
     { id: 3, name: 'Event Charlie', date: '11/20/2025', location: 'Park' }
 ];
 
+let eventRequests =
+[
+    { id: 1, name: 'Event Delta', date: '12/01/2025', time: '10:00 AM' , status: 'Pending' },
+    { id: 2, name: 'Event Echo', date: '12/05/2025', time: '02:00 PM' , status: 'Pending' }
+]
+
 // Handle scheduling events (scheduleEvent.html)
 app.post('/schedule-event', (req, res) => 
 {
@@ -31,7 +37,37 @@ app.get('/events', (req, res) =>
     res.json(events);
 });
 
-app
+app.get("/viewEventRequests", (req, res) =>
+{
+    res.json(eventRequests);
+})
+
+app.post("/approveEvent", (req, res) =>
+{
+    const eventID = req.body.eventID;
+    const eventIndex = eventRequests.findIndex(event => event.id === eventID);
+    if (eventIndex === -1) {
+        return res.status(404).send("Event not found.");
+    }
+    const event = eventRequests(eventIndex);
+    event.status = "Approved";
+    events.push(event);
+    eventRequests.splice(eventIndex, 1);
+    res.send("Event approved.");
+})
+
+app.post("/denyEvent", (req, res) =>
+{
+    const eventID = req.body.eventID;
+    const eventIndex = eventRequests.findIndex(event => event.id === eventID);
+    if (eventIndex === -1) {
+        return res.status(404).send("Event not found.");
+    }
+    const event = eventRequests(eventIndex);
+    event.status = "Denied";
+    eventRequests.splice(eventIndex, 1);
+    res.send("Event denied.");
+})
 
 // Start Express
 app.listen(3000, () => console.log('Running on port 3000'));
