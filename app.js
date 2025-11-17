c4dbc46c7647e44442d602e332d24eaff213b4c3
Muhammad Onais <132248928+onais495@users.noreply.github.com>
2025-11-17 06:46:48 -0500

    switched to port 80

--- app.js
+++ app.js
@@ -225,5 +225,5 @@ app.post("/signup", (req, res) => {
 });
 
 // Start Express
-app.listen(3000, () => console.log('Running on port 3000'));
+app.listen(80, () => console.log('Running on port 80'));
 
